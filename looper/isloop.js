60e5f58cbe22f8be0e7da9ea9d2c3632b04f2ad8
Zachary Ernst <zernst3@live.com>
2020-10-28 16:58:09 -0400

    Completed isLoop

--- looper/isloop.js
+++ looper/isloop.js
@@ -1,10 +1,38 @@
-'use strict';
+"use strict";
 
 //Complete this algo
 const isLoop = (linkedlist) => {
+  let hashForwards = new Map();
+  let hashBackwards = new Map();
+  let currentNodeForwards = linkedlist.head;
+  let currentNodeBackwards = linkedlist.tail;
 
+  while (currentNodeForwards && currentNodeBackwards) {
+    // console.log(
+    //   "This is the hashBackwards Value ",
+    //   hashBackwards[currentNodeBackwards.value]
+    // );
+    // console.log("This is the previous ", currentNodeBackwards.previous);
+    // console.log(
+    //   "This is the hashForwards Value ",
+    //   hashForwards[currentNodeForwards.value]
+    // );
+    if (
+      hashForwards[currentNodeForwards.value] === currentNodeForwards.next ||
+      hashBackwards[currentNodeBackwards.value] ===
+        currentNodeBackwards.previous
+    ) {
+      return true;
+    } else {
+      hashForwards[currentNodeForwards.value] = currentNodeForwards.next;
+      currentNodeForwards = currentNodeForwards.next;
+      hashBackwards[currentNodeBackwards.value] = currentNodeBackwards.previous;
+      currentNodeBackwards = currentNodeBackwards.previous;
+    }
+  }
+
+  return false;
 };
-
 
 /*
 EXTRA CREDIT:
@@ -14,4 +42,4 @@ This function should return the Node value the loop begins at
 Remember to write some test specs too!
 
 */
-module.exports = isLoop
+module.exports = isLoop;
