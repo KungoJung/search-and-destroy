c2da1d489bda8175cbe774895280aab49087faff
Danny Berger <danberadi@gmail.com>
2020-10-28 18:52:56 -0400

    Better notes and console logs

--- min-jumps/min-jumps.js
+++ min-jumps/min-jumps.js
@@ -10,7 +10,7 @@ const minJumps = arr => {
   let currentIdx = 0;
   console.log("CURRENT INDEX>>>>>>>>", currentIdx)
 
-  // looping through the array > while current index < arr.length -1
+  // looping through the array while we're still approaching the right-most element
   while (currentIdx < arr.length-1) {
     // is the end within reach?
     if (arr[currentIdx] >= (arr.length-1) - currentIdx) {
@@ -18,12 +18,12 @@ const minJumps = arr => {
       console.log("______________TOTAL JUMPS:", jumps, "________________")
       return jumps;
     }
-    // pass a subsection of the array to best gains
+    // pass a subsection of the array to resolve the move forward with the best potential gains
     const furthestIdx = currentIdx+arr[currentIdx];
     console.log("FURTHEST AVAILABLE INDEX >>>>>>>>>", furthestIdx )
     console.log("args for best gains:", arr.slice(currentIdx+1, furthestIdx+1))
-    const jumpsToMake = bestGains(arr.slice(currentIdx+1, furthestIdx+1));
-    currentIdx += jumpsToMake;
+    const indexesToJump = bestGains(arr.slice(currentIdx+1, furthestIdx+1));
+    currentIdx += indexesToJump;
     console.log("New Current Index >>>>>>> ", currentIdx)
     jumps++;
     console.log("TOTAL JUMPS >>>>>>", jumps)
@@ -35,26 +35,28 @@ const minJumps = arr => {
 
 const bestGains = (subArray) => {
   console.log("Is SubArray the same? >>>>>>>>", subArray)
+  // default best is the final elem in the subArray
+  let lastIdx = subArray.length-1;
   let best = {
-    indexGain: subArray[subArray.length-1] + subArray.length,
-    value: subArray[subArray.length-1],
-    i: subArray.length-1
+    indexGain: subArray[lastIdx] + subArray.length,
+    value: subArray[lastIdx],
+    i: lastIdx
   }
-
   console.log("Starting Best (last elem in subArray >>>>>>>", best)
 
+  // Loop backwards to see if there's an opportunity for greater index gain
   for (let i = subArray.length-2; i >= 0; i--) {
-    // the current opportunity  is the total of how far along the subArray we are, plus the value stored at the current element in the subArray
+    // the current index gain opportunity  is the total of how far along the subArray we are, plus the value stored at the current element in the subArray
     let value = subArray[i]
     let indexGain = value + i;
     console.log("CURRENT INDEX GAIN OPPORTUNITY >>>>>>>", indexGain)
-    // If that's greater than maxGains, we set maxGains to it
+    // If that's greater than current best indexGain, we set best to the current values:
     if (indexGain > best.indexGain) {
       best = {indexGain, value, i};
       console.log("NEW BEST GAINS >>>>>>>>>>>>", best)
     }
   }
-  // we return the index + 1 because the indexes in the subArray start from idx -0-, but in the minJumps function, the 0th elem of the subArray is a gain of 1 from the current index we're jumping from
+  // the first idx in this subArray is at subArray[0], but in the main func, it's +1 from the currentIdx
   return best.i + 1;
 }
 
