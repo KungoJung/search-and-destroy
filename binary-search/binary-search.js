b787253ff140ad805da0e7598a4fb9cf1a1102a4
Danny Berger <danberadi@gmail.com>
2020-10-28 15:17:18 -0400

    Binary search complete

--- binary-search/binary-search.js
+++ binary-search/binary-search.js
@@ -2,7 +2,15 @@
 
 // Complete this algo
 const binarySearch = (array, target) => {
-	
+	if (array.length == 1 && array[0] !== target) return false
+	const midpoint = Math.floor(array.length / 2)
+
+	if (array[midpoint] === target) return true;
+	else if (array[midpoint] > target) {
+		return binarySearch(array.slice(0, midpoint), target)
+	} else if (array[midpoint] < target) {
+		return binarySearch(array.slice(midpoint), target)
+	}
 };
 
 /*
